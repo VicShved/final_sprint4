65d61d455ddc60e70e70b797a52ad428bd935b6e
Viktor Shvedov <viktor.shvedov@piklema.com>
2026-01-10 10:18:01 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import torch
+from utils import train
+
+
+class Config:
+    # для воспроизводимости
+    SEED = 42
+
+    # Модели
+    TEXT_MODEL_NAME = "bert-base-uncased"
+    IMAGE_MODEL_NAME = "tf_efficientnet_b0"
+
+    # Какие слои размораживаем - совпадают с нэймингом в моделях
+    TEXT_MODEL_UNFREEZE = "encoder.layer.11|pooler"
+    IMAGE_MODEL_UNFREEZE = "blocks.6|conv_head|bn2"
+
+    # Гиперпараметры
+    BATCH_SIZE = 128
+    TEXT_LR = 3e-5
+    IMAGE_LR = 1e-4
+    CLASSIFIER_LR = 1e-3
+    EPOCHS = 30
+    DROPOUT = 0.3
+    HIDDEN_DIM = 256
+    NUM_CLASSES = 4
+
+    # Пути
+    TRAIN_DF_PATH = "data/imdb_train.csv"
+    VAL_DF_PATH = "data/imdb_val.csv"
+    SAVE_PATH = "best_model.pth"
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+cfg = Config()
+
+train(cfg, device)
